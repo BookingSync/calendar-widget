b2ade6f342b5ad1cc4234bdd0d5a7411f4b37418
Romanior <roma.khudyakov@gmail.com>
2016-12-14 20:32:23 +0300

    host and namespace

--- src/config.js
+++ src/config.js
@@ -2,9 +2,10 @@
 
 export default {
   name:    'Calendar Widget',
-  apiHost: NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://www.bookingsync.com/api/v2/public',
+  apiHost: NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://www.bookingsync.com',
+  apiNamespace: '/api/v2/public',
 
   rentalUrl(id) {
-    return NODE_ENV === 'development' ? `${this.apiHost}/rental` : `${this.apiHost}/maps.json?rental_id=${id}`;
+    return NODE_ENV === 'development' ? `${this.apiHost}/rental` : `${this.apiHost}${this.apiNamespace}/maps.json?rental_id=${id}`;
   },
 };
