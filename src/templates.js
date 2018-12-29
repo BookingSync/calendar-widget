5d029d0479244ded3125b85ccf18cab83a4f1802
Okzea <sebastien.garagnani@bookingsync.com>
2018-12-29 01:31:25 +0100

    Fix SVG issues

--- src/templates.js
+++ src/templates.js
@@ -35,7 +35,7 @@ export const weekDay = (label, dis, disStart, isOutAvailable, rate, minStay, rat
 
 export const forward = `
   <button class="${s.forward}" role="button">
-    <svg viewBox="0 0 1000 1000">
+    <svg viewBox="0 0 1000 1000" width="20" height="20">
       <path d="M694.4 242.4l249.1 249.1c11 11 11 21 0 32L694.4 772.7c-5 5-10 7-16 7s-11-2-16-7c-11-11-11-21 0-32l210.1-210.1H67.1c-13 0-23-10-23-23s10-23 23-23h805.4L662.4 274.5c-21-21.1 11-53.1 32-32.1z"></path>
     </svg>
   </button>
@@ -43,7 +43,7 @@ export const forward = `
 
 export const back = `
   <button class="${s.back}" role="button">
-    <svg viewBox="0 0 1000 1000">
+    <svg viewBox="0 0 1000 1000" width="20" height="20">
       <path d="M336.2 274.5l-210.1 210h805.4c13 0 23 10 23 23s-10 23-23 23H126.1l210.1 210.1c11 11 11 21 0 32-5 5-10 7-16 7s-11-2-16-7l-249.1-249c-11-11-11-21 0-32l249.1-249.1c21-21.1 53 10.9 32 32z"></path>
     </svg>
   </button>
