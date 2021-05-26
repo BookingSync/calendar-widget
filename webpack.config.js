486dc31e628baba27a72b1621576836bec8052be
Okzea <Okzea@live.fr>
2021-05-26 13:07:57 +0700

    lint

--- webpack.config.js
+++ webpack.config.js
@@ -51,9 +51,9 @@ const config = {
   optimization,
   devServer: {
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
     }
   },
   entry: `${__dirname}/src/${name}.js`,
